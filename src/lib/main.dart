d23bc81899f4e1b405fbf05353fdbd9b741dcd1e
Alex221100 <71218218+Alex221100@users.noreply.github.com>
2022-12-05 20:34:28 +0100

    feat(top menu): create top menu with logo and icons.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:ecodot/top_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:dot_navigation_bar/dot_navigation_bar.dart';
-import 'BottomNavBar.dart';
+import 'BottomNavbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Ecodot',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60),
+            child: TopMenu(),
+          ),
+          bottomNavigationBar: BottomNavBar(0)),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(),
-        extendBody: true,
-        bottomNavigationBar: const BottomNavBar(0));
   }
 }
---
+++ src/lib/top_menu.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class TopMenu extends StatefulWidget {
+  const TopMenu({Key? key}) : super(key: key);
+
+  @override
+  _TopMenu createState() => _TopMenu();
+}
+
+class _TopMenu extends State<TopMenu> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const SizedBox(
+        width: 70,
+        height: 70,
+        child: Center(
+          child: Image(image: AssetImage('../assets/ecodot.png')),
+        ),
+      ),
+      backgroundColor: const Color(0xffF4F5FA),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: IconButton(
+            icon: const Icon(Icons.dark_mode),
+            color: const Color.fromRGBO(83, 78, 89, 0.8),
+            onPressed: () {},
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: IconButton(
+            icon: const Icon(Icons.notifications),
+            color: const Color.fromRGBO(83, 78, 89, 0.8),
+            onPressed: () {},
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('../assets/avatar-default.png'),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
