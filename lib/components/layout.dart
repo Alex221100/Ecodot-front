b5d45bbd919a7bf488ea5fe69f46225e2d871f40
ASzmundy <58273758+ASzmundy@users.noreply.github.com>
2022-12-12 12:34:49 +0100

    Début du front calculette + classe de décoration générique + fix chargement images layout + backgroundcolor white sur le layout

--- lib/components/layout.dart
+++ lib/components/layout.dart
@@ -17,6 +17,7 @@ class _MyLayout extends State<MyLayout> {
           preferredSize: Size.fromHeight(60),
           child: TopMenu(),
         ),
+        backgroundColor: Colors.white,
         body: widget.child);
   }
 }
--- lib/screens/calculation.dart
+++ lib/screens/calculation.dart
@@ -1,6 +1,10 @@
 import 'package:ecodot/components/layout.dart';
 import 'package:flutter/material.dart';
 
+import '../components/inputfields.dart';
+
+//TODO Tarif du kWh persisté (cache/sharedpreferences)
+
 class Calculation extends StatefulWidget {
   const Calculation({super.key});
 
@@ -9,8 +13,65 @@ class Calculation extends StatefulWidget {
 }
 
 class _Calculation extends State<Calculation> {
+  String devise = "€";
+  Color devisetextcolor = Colors.grey;
+
   @override
   Widget build(BuildContext context) {
-    return const MyLayout(child: Text("Calculation"));
+    return MyLayout(
+        child: Column(children: [
+      Container(
+          alignment: Alignment.topCenter,
+          padding: EdgeInsets.only(top: 25, bottom: 25),
+          child: const Text("Calculer la consommation électrique",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
+      Container(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+            //Champ coût
+            Container(
+              padding: EdgeInsets.only(top: 12),
+              width: 150,
+              child: new TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  decoration: new InputFieldDecorationGeneric1('Tarif du kWh')),
+            ),
+            SizedBox(width: 15), //Espace entre les 2
+                //Champ devise
+            Stack(children: [
+              Container(
+                padding: EdgeInsets.only(top: 12),
+                  width: 150,
+                  child: DropdownButtonFormField<String>(
+                    value: devise,
+                    alignment: AlignmentDirectional.center,
+                    items: [
+                      DropdownMenuItem(
+                        value: "€",
+                        child: Text("€"),
+                      ),
+                      DropdownMenuItem(
+                        value: "\$",
+                        child: Text("\$"),
+                      )
+                    ],
+                    decoration: InputFieldDecorationGeneric1(""),
+                    onChanged: (String? value) {
+                      devise = value!;
+                    }
+                    ,
+                  ))
+              ,Positioned(
+                  left: 11,
+                  top: 6,
+                  child: Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      color: Colors.white,
+                      child: Text("Unité",
+                          style: TextStyle(color: devisetextcolor, fontSize: 12)))),
+            ])
+          ]))
+    ]));
   }
 }
