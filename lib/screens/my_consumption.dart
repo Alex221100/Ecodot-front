88282bc11d3606ec28251c7bf26a3b921c6b3513
Pr0t0 <mathis.fossey@outlook.com>
2023-01-16 08:24:22 +0100

    ajout du carousel avec les graphiques de consommation

--- lib/screens/my_consumption.dart
+++ lib/screens/my_consumption.dart
@@ -33,52 +33,75 @@ class _MyConsumption extends State<MyConsumption> {
                       blurRadius: 7,
                       offset: const Offset(0, 3))
                 ]),
-            child: Column(children: [
-              Container(
-                  alignment: Alignment.topCenter,
-                  padding: EdgeInsets.only(top: 25, bottom: 25),
-                  child: const Text("Consommation moyenne",
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DoubleValueTextWithCircle(
-                consumption: 4.5,
-                currency: '€',
-                priceInCents: 4.5 * 27,
-                foregroundColor: const Color(0xff56CA00),
-                maxValue: 6,
-                unit: Unit.kWh,
+            child: CarouselSlider.builder(
+              options: CarouselOptions(
+                height: 272.0,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+                enableInfiniteScroll: false,
               ),
-            ]),
+              itemCount: 4,
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      Column(
+                children: [
+                  Container(
+                      alignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(top: 25, bottom: 25),
+                      child: const Text(
+                          "Consommation de la journée :", //mettre un if si index = 1 2 3 4
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                  const DoubleValueTextWithCircle(
+                    consumption: 40.5, //pareil ici mettre un if
+                    currency: '€',
+                    priceInCents: 40.5 * 27, //pareil ici mettre un if
+                    foregroundColor: const Color(0xff56CA00),
+                    maxValue: 100,
+                    unit: Unit.kWh,
+                  ),
+                ],
+              ),
+            ),
           )),
-      Spacer(),
       FractionallySizedBox(
-          widthFactor: 0.9,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 3))
-                ]),
-            child: Column(children: [
+        widthFactor: 0.9,
+        child: Container(
+          margin: EdgeInsets.only(top: 25),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3))
+              ]),
+          child: Row(
+            children: [
               Container(
-                  alignment: Alignment.topCenter,
-                  padding: EdgeInsets.only(top: 25, bottom: 25),
-                  child: const Text("Consommation moyenne",
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DoubleValueTextWithCircle(
-                consumption: 4.5,
-                currency: '€',
-                priceInCents: 4.5 * 27,
-                foregroundColor: const Color(0xff56CA00),
-                maxValue: 6,
-                unit: Unit.kWh,
+                padding: EdgeInsets.only(left: 10),
+                child: Icon(Icons.help_outline,
+                    size: 50, color: const Color(0xff56CA00)),
               ),
-            ]),
-          )),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: const LimitedBox(
+                    maxWidth: 280,
+                    maxHeight: 100,
+                    child: Text(
+                      'Vous souhaitez réduire votre consommation ? \nSuivez notre guide de bonnes pratiques sur les ressources énergétiques à la maison.',
+                      textAlign: TextAlign.justify,
+                    )),
+              ),
+            ],
+          ),
+        ),
+      )
     ]));
   }
 }
