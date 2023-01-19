3905d4a81a8b769b61886092600bd08a71b093e2
dabarik <thierryxu60@gmail.com>
2023-01-19 11:50:38 +0100

    notifications onclick

--- lib/components/top_menu.dart
+++ lib/components/top_menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 //call notification class in top_menu.dart
-import 'package:ecodot/screens/notification.dart' as notification;
 
 class TopMenu extends StatefulWidget {
   const TopMenu({super.key});
@@ -21,24 +20,14 @@ class _TopMenu extends State<TopMenu> {
         ),
       ),
       actions: [
-        IconButton(
-          icon: const Icon(
-            Icons.notifications,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const notification.Notification()),
-            );
-          },
-        ),
         Padding(
           padding: const EdgeInsets.only(right: 10),
           child: IconButton(
             icon: const Icon(Icons.notifications),
             color: const Color.fromRGBO(83, 78, 89, 0.8),
-            onPressed: () {},
+            onPressed: () async {
+              await showNotification();
+            },
           ),
         ),
         Padding(
@@ -57,6 +46,64 @@ class _TopMenu extends State<TopMenu> {
         )
       ],
       elevation: 0,
+    );
+  }
+
+  Future<void> showNotification() async {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    //create notification channel
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      importance: Importance.high,
+    );
+    //initialize notification channel
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+//initialize notification settings
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('ecodot_with_name');
+
+    DarwinInitializationSettings initializationSettingsDarwin =
+        const DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    InitializationSettings initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: initializationSettingsDarwin);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+//create notification details
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your channel id', // id
+      'your channel name', // title
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    //create notification details ios with darwin
+    const DarwinNotificationDetails darwinPlatformChannelSpecifics =
+        DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: darwinPlatformChannelSpecifics);
+//show notification
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Veuillez dégivrer votre réfrigérateur',
+      'Faites attention à votre consommation d\'énergie',
+      platformChannelSpecifics,
+      payload: 'item x',
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,13 +4,11 @@
 
 import FlutterMacOS
 import Foundation
-import shared_preferences_foundation
+
 import flutter_local_notifications
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-}
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
